ff21777eb194de0eac6e616d6663049633d373bd
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2019-04-29 00:33:16 +0500

    Continue

--- src/App.js
+++ src/App.js
@@ -3,7 +3,51 @@
 import React,{Component} from "react";
 import './App.css';
 
+const formValid= formErrors=>{
+  let valid=true;
+  Object.values(formErrors).forEach( val => {val.length > 0 && (valid= false)});
+  return valid;
+};
+
 class App extends Component{
+  constructor (props){
+    super(props);
+
+    this.state={
+      firstName: null,
+      lastName: null,
+      password: null,
+      email: null,
+      formErrors:{
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:"" 
+      }
+    };
+  }
+  handlesubmit = e =>{
+    e.preventDefault();
+
+    if (formValid(this.state.formErrors)){
+      console.log(`--SUBMITTING-- First Name: ${this.state.firstName} Last Name: ${this.state.lastName}
+      Email: ${this.state.email} Password: ${this.state.password}  `)
+    } else{
+      console.error('FORM INVALID - DISPLAY ERROR MESSAGE ')
+    }
+  };
+
+  handleChange = e =>{
+    e.preventDefault();
+    const{name, value}= e.target;
+    let formErrors= this.state.formErrors;
+
+    switch(name){
+      case 'firstName':
+      formErrors.firstName = value.length < 3 && value.length > 0 ? 'minimum 3 characaters required': "";
+      break;
+    }
+  };
   render(){
     return(
       <div className="wrapper">
@@ -20,18 +64,22 @@ class App extends Component{
       </div>
       <div className="email">
       <label htmlFor="email">Email: </label>
-      <input type="text" className="" placeholder="Email" type="text" name="email" noValidate onChange={this.handleChange}/>
+      <input type="email" className="" placeholder="Email" type="text" name="email" noValidate onChange={this.handleChange}/>
       </div>
       <div className="password">
       <label htmlFor="password">Password: </label>
       <input type="password" className="" placeholder="Password" type="password" name="password" noValidate onChange={this.handleChange}/>
       </div>
+      <div className="createAccount">
+      <button type="submit">Create an Account</button>
+      <small>Already have an Account?</small>
+      </div>
       </form>
       </div>
       </div>
     );
-  }
-}
+  };
+};
 
 // function App() {
 //   return (
